97c7736ac646d091916a787d761253c3b7c33a13
Zaki <zakrzes1994@op.pl>
2016-10-24 15:51:51 +0200

    commit2

---
+++ Program4/Osoba.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class Osoba
+    {
+        protected string imie;
+        protected string nazwisko;
+        protected int rokUr;
+        private string miejsceZamieszkania;
+
+        // konstruktor domyslny
+        public Osoba()
+        {
+            imie = "Jan";
+            nazwisko = "Kowalski";
+            rokUr = 1996;
+        }
+
+        protected internal Osoba(string imie, string nazwisko, int rokUr)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rokUr = rokUr;
+        }
+
+        protected internal void WypiszInfo()
+        {
+            Console.WriteLine("Imie i nazwisko: {0} {1}", imie, nazwisko);
+            Console.WriteLine(rokUr);
+        }
+
+        protected internal void ObliczWiek()
+        {
+            Console.WriteLine("Wiek: {0}", DateTime.Now.Year - rokUr);
+        }
+
+        //metoda ustawiajaca miejsce zamieszkania
+        protected internal void UstawMiejsceZamieszkania(string miejsceZamieszkania)
+        {
+            this.miejsceZamieszkania = miejsceZamieszkania;
+        }
+
+    }
+}
---
+++ Program4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+           /* Osoba lukasz = new Osoba("Lukasz", "Zakrzewski", 1994);
+            Osoba osoba1 = new Osoba();
+            Student student = new Student("Krzysiu","Wi",1997,3,5,134444);
+            student.WypiszInfo();
+            osoba1.WypiszInfo();
+            lukasz.WypiszInfo();
+            lukasz.ObliczWiek();
+            student.ObliczWiek();
+            student.UstawMiejsceZamieszkania("Olsztyn"); */
+            
+            Osoba student2 = new Student();
+            student2.WypiszInfo();
+            Student student3 = new Student();
+            Osoba osoba3 = student3;
+            Console.ReadKey();
+
+        }
+    }
+}
